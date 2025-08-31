c26623dd4465ac92abc63c2753ed6c2f74e9d5d4
livio <livio@uber.com>
2025-08-30 20:26:07 -0700

    player moves, udpate and draw by groups

--- main.py
+++ main.py
@@ -11,15 +11,22 @@ def main():
     dt =0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatables = pygame.sprite.Group()
+    drawables  = pygame.sprite.Group()
+    Player.containers = (updatables , drawables)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatables.update(dt)
         screen.fill(color='black')
-        player.update(dt)
-        player.draw(screen)
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
