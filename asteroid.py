8e4591d88b3944f453e43f8d010e4e98d871df3a
livio <livio@uber.com>
2025-09-01 14:22:39 -0700

    shot splits but random quits

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 from constants import * 
 from circleshape import CircleShape
 
@@ -9,3 +9,11 @@ class Asteroid(CircleShape):
         pygame.draw.circle(screen, color='white', center=self.position, radius=self.radius, width=2)
     def update(self, dt):
         self.position += self.velocity * dt
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20, 50)
+            asteroid_1, asteroid_2 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS), Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS),
+            asteroid_1.velocity, asteroid_2.velocity = self.velocity.rotate(angle)*1.2, self.velocity.rotate(-angle)*1.2
--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
                 return
             for shot in shots:
                 if shot.collide(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill(color='black')
