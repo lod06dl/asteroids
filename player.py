ed11a955622001e89473cb42bdd203954d46574c
livio <livio@uber.com>
2025-09-01 13:49:18 -0700

    we're shoot every .3

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, radius= PLAYER_RADIUS)
         self.rotation = 0
+        self.player_shot_cooldown = 0
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -20,6 +21,7 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     def update(self, dt):
+        self.player_shot_cooldown -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -37,5 +39,9 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt 
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)*PLAYER_SHOT_SPEED
+        if self.player_shot_cooldown >0:
+            return
+        else:
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)*PLAYER_SHOT_SPEED
+            self.player_shot_cooldown = PLAYER_SHOOT_COOLDOWN
